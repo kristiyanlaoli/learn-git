4b05e0a573705a6788fd6bd585274345c8a52299
Kristiyan <kristiyanlaoli@gmail.com>
2024-02-17 15:31:44 +0700

    created checkout Button

---
+++ src/components/micro/ButtonCheckout.jsx
@@ -0,0 +1,9 @@
+let ButtonCheckout = () => {
+  return (
+    <>
+   <button className="font-bold text-xl text-color-light border-2 border-color-secondary p-2 bg-color-primary hover:bg-color-secondary rounded-xl">Checkout</button>
+    </>
+  );
+};
+
+export default ButtonCheckout;
---
+++ src/pages/CheckoutPage.jsx
@@ -0,0 +1,14 @@
+import ButtonCheckout from "../components/micro/ButtonCheckout";
+import ButtonPlusMinus from "../components/micro/ButtonPlusMinus";
+
+let CheckoutPage = () => {
+  return (
+    <>
+      <ButtonPlusMinus />
+      <br />
+      <ButtonCheckout />
+    </>
+  );
+};
+
+export default CheckoutPage;
