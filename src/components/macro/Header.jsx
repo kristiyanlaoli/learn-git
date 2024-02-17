b29d73fc5d9a200864e65475915a5e41a788daef
Kristiyan <kristiyanlaoli@gmail.com>
2024-02-17 14:31:16 +0700

    created header

---
+++ src/components/macro/Header.jsx
@@ -0,0 +1,13 @@
+let Header = () => {
+  return (
+    <div className="p-4 bg-color-primary flex items-center justify-between">
+      <h1 className="flex ml-[40px]">
+        <div className="font-bold text-3xl text-color-light">Shop</div>
+        <div className="font-bold text-3xl text-color-secondary">Sphere</div>
+      </h1>
+      <p className="font-bold text-color-light mr-6">Login</p>
+    </div>
+  );
+};
+
+export default Header;
